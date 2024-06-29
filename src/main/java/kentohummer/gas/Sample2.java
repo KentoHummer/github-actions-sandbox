095c34793f601d3d058708a7e4877d5c0b75998d
KentoHummer <hummer3150bass@gmail.com>
2024-06-29 10:10:35 +0900

    ✨ 追加更新

--- src/main/java/kentohummer/gas/Sample2.java
+++ src/main/java/kentohummer/gas/Sample2.java
@@ -3,5 +3,7 @@ package kentohummer.gas;
 public class Sample2 {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        System.out.println("line2");
+        System.out.println("line3");
     }
 }
